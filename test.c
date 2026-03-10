7ae1dc9c64ed8a3cd452e1f966bed3341c0fdbc0
UlvLe <3785353139@qq.com>
2026-03-10 13:47:47 +0800

    first

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world\n");
+    printf("here");
+
+    int a = 0;
+    scanf("%d",&a);
+    double pi = 3.14159;
+    double s = pi*a*a;
+    printf("圆面积是%f",s);
+    
+    return 0;
+}
